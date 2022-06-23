3289fa99daf6288f37e90465267140351895c906
Oluwafisayo-Eyitayo <lurveth@gmail.com>
2022-06-23 18:35:10 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# java-ass
+ ass
---
+++ ass.js
@@ -0,0 +1,8 @@
+let firstname = "Oluwafisayo";
+let lastname = "Eyitayo";
+let height =  "5.7";
+let country = "Nigeria";
+console.log (firstname);
+console.log (lastname);
+console.log (height);
+console.log (country);
